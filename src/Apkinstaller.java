ca52b438bb5a7418daf5e056f15841326936ad74
Aniket Patil <aniketp563@gmail.com>
2016-03-17 12:05:03 +0530

    Add the File Push Button and Get Product name in device check

--- src/Apkinstaller.java
+++ src/Apkinstaller.java
@@ -43,6 +43,15 @@ import javax.swing.ScrollPaneConstants;
 import javax.swing.border.TitledBorder;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.LineBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JSeparator;
+import java.awt.SystemColor;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Apkinstaller extends JFrame {
 
@@ -90,7 +99,7 @@ public class Apkinstaller extends JFrame {
 	
 	public Apkinstaller() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 806, 379);
+		setBounds(100, 100, 804, 384);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -105,7 +114,7 @@ public class Apkinstaller extends JFrame {
         
         JProgressBar progressBar = new JProgressBar(0,100);
         progressBar.setForeground(Color.DARK_GRAY);
-        progressBar.setBounds(10, 315, 770, 14);
+        progressBar.setBounds(10, 321, 770, 14);
         contentPane.add(progressBar);
         
         
@@ -126,6 +135,7 @@ public class Apkinstaller extends JFrame {
         
         //Install Button
         btnNewButton = new JButton("Install");
+        btnNewButton.setFocusPainted(false);
         btnNewButton.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent arg0) {
         		
@@ -233,14 +243,37 @@ public class Apkinstaller extends JFrame {
         
         //This shows the status that device is attached or not 
         btnDeviceCheck = new JButton("Device Check");
+        btnDeviceCheck.setFocusPainted(false);
         btnDeviceCheck.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent arg0) {
-        			
+        		
+        		//Check ADB connection
         		ProcessBuilder pb=new ProcessBuilder("adb","devices");
         		Process p;
 				try {
 					p =pb.start();
 					InputStream s = p.getInputStream();
+					
+					BufferedReader in = new BufferedReader(new InputStreamReader(s));
+					
+					String temp;
+
+	        		while ((temp = in.readLine()) != null) {
+	        			
+	        		    System.out.println(temp);
+	        		    textCommandStatus.append(temp+"\n");   
+	        		}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				//Roar the device model
+				ProcessBuilder pm=new ProcessBuilder("adb","shell","getprop","ro.product.name");
+        		Process o;
+				try {
+					o =pm.start();
+					InputStream s = o.getInputStream();
 					
 					BufferedReader in = new BufferedReader(new InputStreamReader(s));
 					
@@ -268,7 +301,7 @@ public class Apkinstaller extends JFrame {
         JScrollPane scrollPane_1 = new JScrollPane();
         scrollPane_1.setViewportBorder(new LineBorder(Color.WHITE));
         scrollPane_1.setToolTipText("CMD VIEW\r\n");
-        scrollPane_1.setBounds(349, 92, 431, 184);
+        scrollPane_1.setBounds(349, 92, 431, 218);
         contentPane.add(scrollPane_1);
         
         textCommandStatus = new JTextArea();
@@ -283,6 +316,7 @@ public class Apkinstaller extends JFrame {
         contentPane.add(lblCommandLineView);
         
         JButton btnReset = new JButton("Reset");
+        btnReset.setFocusPainted(false);
         btnReset.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent arg0) {
         		
@@ -303,6 +337,7 @@ public class Apkinstaller extends JFrame {
         scrollPane.setViewportView(listApk);
         
         JButton btnRemove = new JButton("-");
+        btnRemove.setFocusPainted(false);
         btnRemove.setFont(new Font("Tahoma", Font.ITALIC, 18));
         btnRemove.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent arg0) {
@@ -328,6 +363,7 @@ public class Apkinstaller extends JFrame {
         contentPane.add(btnRemove);
         
         JButton btnNewButton_1 = new JButton("+");
+        btnNewButton_1.setFocusPainted(false);
         btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 11));
         btnNewButton_1.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent arg0) {
@@ -363,13 +399,115 @@ public class Apkinstaller extends JFrame {
         contentPane.add(btnNewButton_1);
         
         JButton btnAboutMe = new JButton("About Me");
+        btnAboutMe.setFocusPainted(false);
         btnAboutMe.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent arg0) {
         		
         		JOptionPane.showMessageDialog(null, "APK BATCH INSTALLER"+"\nDevloped by"+"\nAniket Patil"+"\naniketp563@gmail.com");
         	}
         });
+        
         btnAboutMe.setBounds(671, 41, 109, 23);
         contentPane.add(btnAboutMe);
+        
+        
+        
+        //PUSH FILES
+        JButton btnNewButton_2 = new JButton("Push Files");
+        btnNewButton_2.setFocusPainted(false);
+        btnNewButton_2.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent arg0) {
+        		
+
+        		System.out.println("\n Debug Push Files");
+        		
+        		progressBar.setIndeterminate(false);
+        		progressBar.setMinimum(0);
+        		progressBar.setMaximum(100);
+        		progressBar.setVisible(true);
+        		
+        		
+        		
+                // We're going to do something that takes a long time, so we
+                // spin off a thread and update the display when we're done.
+                Thread worker = new Thread() {
+                  public void run() {
+                    // Something that takes a long time . . . in real life,
+                    // this
+                    // might be a DB query, remote method invocation, etc.
+                    try {
+                    	
+                    	progressBar.setIndeterminate(true);
+                    	
+                    	//Dummy
+                	    for (Iterator iterator = arrayList.iterator(); iterator.hasNext();) {
+        					String string = (String) iterator.next();
+        					System.out.println(string);
+        					
+
+        					try {
+        						
+        						ProcessBuilder pb = new ProcessBuilder("adb", "push",string,"/sdcard/");
+        						Process pc = null;
+        						
+        						pc = pb.start();
+        						
+        						
+        						InputStream s = pc.getInputStream();
+        						
+        						BufferedReader in = new BufferedReader(new InputStreamReader(s));
+        						
+        						String temp;
+        						
+        						
+        						
+                                //While 
+        		        		while ((temp = in.readLine()) != null) {
+        		        			
+        		        		    System.out.println(temp);
+        		        		    textCommandStatus.append(temp+"\n");;
+        						
+        						    int exitvalue=pc.waitFor();
+        						
+        						    System.out.println(exitvalue);
+        					        System.out.println("Done");
+        					        
+        					        				        
+        					     }
+        		        		//While end
+        					   }catch (IOException | InterruptedException e) {
+        						// TODO Auto-generated catch block
+        						e.printStackTrace();
+        					}
+        		        }
+                	   flag=1;
+                      Thread.sleep(5000);
+                    } catch (InterruptedException ex) {
+                    }
+                    
+                    
+                       
+                    // Report the result using invokeLater().
+                    SwingUtilities.invokeLater(new Runnable() {
+                      public void run() {
+                    	  
+                       textCommandStatus.repaint();
+                       progressBar.repaint();
+                       
+                       if(flag==1)
+                       {
+                    	   progressBar.setIndeterminate(false);
+                       }
+               
+                      }
+                    });
+                  }
+                };
+
+              worker.start(); // So we don't hold up the dispatch thread.
+     	}
+        });
+        btnNewButton_2.setBounds(514, 10, 117, 23);
+        contentPane.add(btnNewButton_2);
 	}
 }
